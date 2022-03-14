fcdad1eed119a61658948678ddee7f79e2efdfe3
vishwajeet009 <56509693+vishwajeet009@users.noreply.github.com>
2022-03-14 12:28:23 +0530

    Create admin.py

---
+++ ems_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from ems_app import models
+
+
+# Register your models here.
+admin.site.register(models.EntranceExitReason)
+admin.site.register(models.EntranceExitModel)
