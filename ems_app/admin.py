3a74745a1a06ce98f0ca97b9ff0e3fcd5b6ca70e
vishwajeet009 <56509693+vishwajeet009@users.noreply.github.com>
2022-04-26 18:41:12 +0530

    Delete admin.py

--- ems_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from ems_app import models
-
-
-# Register your models here.
-admin.site.register(models.EntranceExitReason)
-admin.site.register(models.EntranceExitModel)
